d7c2b8f1788b2ba9888259fcb320e89e78905c3d
techwebdots <techwebdots@gmail.com>
2025-07-09 00:58:18 +0530

    Access Storage Account

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union
 from fastapi import FastAPI
 import os
 from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient
+from azure.core.exceptions import AzureError
 
 # Load environment variables from .env file
 load_dotenv()
@@ -28,3 +30,21 @@ print(f"API Key: {api_key}")
 @app.get("/second", status_code=200, response_description="Health Check")
 def read_health():
     return {"status": "OK", "message": f"Service2 is running  {api_key}"}
+
+# Access the variables using os.getenv()
+connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+
+@app.get("/getsafilename", response_description="Get Storage Account File Name")
+def read_safilename():
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+        container_name = "blob-container"
+        container_client = blob_service_client.get_container_client(container_name)
+
+        print("\n getting blobs...")
+        blob_list = container_client.list_blobs()
+        for blob in blob_list:
+            return {f"I see the first blob name is -> {blob.name}"}
+    except AzureError as e:
+        print(f"Failed to connect or list blob: {e}")
